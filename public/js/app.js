5210307ecab4a19e4ef6edb51bc39e9353de8032
Christian Paul Flores <florespaul161@gmail.com>
2022-10-13 14:56:51 +0800

    test

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ function tableSearch() {
     tr = table.getElementsByTagName('tr');
 
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName('td')[2]
+        td = tr[i].getElementsByTagName('td')[1]
 
         if (td) {
             txtValue = td.textContent || td.innerText;
